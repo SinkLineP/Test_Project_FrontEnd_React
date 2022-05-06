8c04e757b0c392b2c7302a039dfe8ae9ead42806
SinkLine_P <popovartem505@gmail.com>
2022-05-06 23:08:45 +0300

    edit a link

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -31,7 +31,7 @@ const Pagination = (
           {
             pageNumbers.map((number) => (
               <li className={"page-item"} key={number}>
-                <a href={"#" + number} onClick={() => paginate(number)}>
+                <a onClick={() => paginate(number)}>
                   {number}
                 </a>
               </li>
